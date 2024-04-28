56569ae160048a1c529384e48dbc0bcde246fb2c
kanazholdasov <156598163+kanazholdasov@users.noreply.github.com>
2024-04-28 22:28:35 +0500

    Stack

---
+++ src/MyStack.java
@@ -0,0 +1,29 @@
+public class MyStack<K> extends MyArrayList<K>{
+    MyArrayList<K> arrayList;
+
+    public MyStack(){
+        arrayList = new MyArrayList<K>();
+    }
+
+    public void push(K item){
+        arrayList.addFirst(item);
+    }
+
+    public K peek(){
+        return arrayList.getFirst();
+    }
+
+    public K pop(){
+        K removingItem = peek();
+        arrayList.removeFirst();
+        return removingItem;
+    }
+
+    public int size(){
+        return arrayList.size();
+    }
+
+    public boolean isEmpty(){
+        return size() == 0;
+    }
+}
