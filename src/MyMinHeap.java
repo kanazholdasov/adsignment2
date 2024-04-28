bbeb276ce00aa482faeb523f9df765c4c7511888
kanazholdasov <156598163+kanazholdasov@users.noreply.github.com>
2024-04-28 22:44:44 +0500

    Minheap

--- src/MyMinHeap.java
+++ src/MyMinHeap.java
@@ -1,18 +1,22 @@
 public class MyMinHeap<K extends Comparable<K>>{
     private MyArrayList<K> heap;
 
+    // Constructor
     public MyMinHeap() {
         heap = new MyArrayList<>();
     }
 
+    // Check if the heap is empty
     public boolean isEmpty() {
         return size() == 0;
     }
 
+    // Get the size of the heap
     public int size() {
         return heap.size();
     }
 
+    // Get the minimum element in the heap
     public K getMin() {
         if (isEmpty()) {
             throw new IllegalStateException("Heap is empty");
@@ -20,6 +24,7 @@ public class MyMinHeap<K extends Comparable<K>>{
         return heap.get(0);
     }
 
+    // Extract the minimum element from the heap
     public K extractMin() {
         if (isEmpty()) {
             throw new IllegalStateException("Heap is empty");
@@ -31,11 +36,13 @@ public class MyMinHeap<K extends Comparable<K>>{
         return min;
     }
 
+    // Insert an element into the heap
     public void insert(K item) {
         heap.add(item);
         traverseUp(heap.size() - 1);
     }
 
+    // Restore the heap property starting from the given index
     private void heapify(int index) {
         int left = leftChildOf(index);
         int right = rightChildOf(index);
@@ -55,6 +62,7 @@ public class MyMinHeap<K extends Comparable<K>>{
         }
     }
 
+    // Move an element up in the heap until the heap property is satisfied
     private void traverseUp(int index) {
         while (index > 0 && heap.get(parentOf(index)).compareTo(heap.get(index)) > 0) {
             swap(index, parentOf(index));
@@ -62,24 +70,29 @@ public class MyMinHeap<K extends Comparable<K>>{
         }
     }
 
+    // Get the index of the left child of the given index
     private int leftChildOf(int index) {
         return 2 * index + 1;
     }
 
+    // Get the index of the right child of the given index
     private int rightChildOf(int index) {
         return 2 * index + 2;
     }
 
+    // Get the index of the parent of the given index
     private int parentOf(int index) {
         return (index - 1) / 2;
     }
 
+    // Swap two elements in the heap
     private void swap(int index1, int index2) {
         K temp = heap.get(index1);
         heap.set(index1, heap.get(index2));
         heap.set(index2, temp);
     }
 
+    // Method to print the elements of the heap
     public void printArr(){
         heap.printArr();
     }
