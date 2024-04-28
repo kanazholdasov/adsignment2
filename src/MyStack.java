dde9fa546700bc6e11df53734ac8c0df25a31fc7
kanazholdasov <156598163+kanazholdasov@users.noreply.github.com>
2024-04-28 22:44:59 +0500

    Stack

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,28 +1,35 @@
+// MyStack class represents a stack data structure implemented using an ArrayList
 public class MyStack<K> {
-    MyArrayList<K> arrayList;
+    MyArrayList<K> arrayList; // ArrayList to store the elements of the stack
 
+    // Constructor to initialize the ArrayList
     public MyStack(){
         arrayList = new MyArrayList<K>();
     }
 
+    // Method to push an element onto the stack
     public void push(K item){
         arrayList.addFirst(item);
     }
 
+    // Method to get the element at the top of the stack without removing it
     public K peek(){
         return arrayList.getFirst();
     }
 
+    // Method to pop and return the element at the top of the stack
     public K pop(){
-        K removingItem = peek();
-        arrayList.removeFirst();
-        return removingItem;
+        K removingItem = peek(); // Get the element at the top of the stack
+        arrayList.removeFirst(); // Remove the element from the top of the stack
+        return removingItem; // Return the removed element
     }
 
+    // Method to return the size of the stack
     public int size(){
         return arrayList.size();
     }
 
+    // Method to check if the stack is empty
     public boolean isEmpty(){
         return size() == 0;
     }
